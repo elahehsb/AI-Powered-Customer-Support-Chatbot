c85670fa6cd983e9b7acd89e91123be01454cdc2
Elahe Sabbaghi <82085007+elahehsb@users.noreply.github.com>
2024-07-23 20:23:38 +0330

    Create app.py
    a web API using Flask to serve the chatbot model

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
+# Load the model and vectorizer
+with open('chatbot_model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+with open('vectorizer.pkl', 'rb') as file:
+    vectorizer = pickle.load(file)
+
+app = Flask(__name__)
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_message = request.json['message']
+    user_message_transformed = vectorizer.transform([user_message])
+    
+    response = model.predict(user_message_transformed)
+    return jsonify({'response': response[0]})
+
+if __name__ == '__main__':
+    app.run(debug=True)
